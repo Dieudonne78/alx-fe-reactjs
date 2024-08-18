4085478698cff98018b696151b3e330cbba75009
Dieudonne <dieudonnek077@gmail.com>
2024-08-18 20:24:30 +0200

    Build a Simple Company Website with React

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div style={{display:'flex',justifyContent:'center',gap:'10px'}}>
+    <div style={{display:'flex',justifyContent:'center',gap:'10px',backgroundColor:'black',color:'white'}}>
       <Link to='/'>Home</Link>
       <Link to='/About'>About</Link>
       <Link to='/Services'>Services</Link>
