0c66ffbafa6180443e60ae0d54e2a5dec32d50d0
Dieudonne <dieudonnek077@gmail.com>
2024-08-24 11:55:04 +0200

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,7 +1,12 @@
+import { useRecipeStore } from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 function Delete(){
+  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const navigate = useNavigate();
+  navigate("/")
   return(
     <div>
-      Delete
+      <button type="submit">Delete Recipe</button>
     </div>
   )
 }
