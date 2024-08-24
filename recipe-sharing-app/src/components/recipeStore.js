26f715923c8b55cde6b5b1dc482d4bf8cc440489
Dieudonne <dieudonnek077@gmail.com>
2024-08-24 11:43:57 +0200

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,9 @@ import {create} from 'zustand'
 export const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  updateRecipe:set(state => ({ recipes: [...state.recipes, updateRecipe] })),
+  deleteRecipe:set(state => ({ recipes: [...state.recipes, deleteRecipe] }))
 }));
 
 
