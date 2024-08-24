0e2ba9b4626265d58ff8488aae4b45b3aca9159a
Dieudonne <dieudonnek077@gmail.com>
2024-08-24 11:59:13 +0200

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Route,Routes } from 'react-router-dom';
+import { Route,Routes,Router } from 'react-router-dom';
 import './App.css'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
