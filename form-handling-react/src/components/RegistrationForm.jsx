932da3098574033819e7c13c5bc8244c715b1b8c
Dieudonne <dieudonnek077@gmail.com>
2024-09-01 11:41:13 +0200

    Implementing Form Handling in React with Controlled Components and Formik

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -28,9 +28,9 @@ function Registration(){
    <>
    <form onSubmit={formik.handleSubmit}>
 
-    <input type="text" name='username' placeholder="username" value={formik.values.username}  onChange={formik.handleChange}/>
-    <input type="email" name='email' placeholder="email" value={formik.values.email} onChange={formik.handleChange}/>
-    <input type="password" name='password' placeholder="password" value={formik.values.password} onChange={formik.handleChange}/>
+    <input type="text" name='username' placeholder="username" value={username}  onChange={formik.handleChange}/>
+    <input type="email" name='email' placeholder="email" value={email} onChange={formik.handleChange}/>
+    <input type="password" name='password' placeholder="password" value={password} onChange={formik.handleChange}/>
     <button type='submit'>Submit</button>
    </form>
    </>
