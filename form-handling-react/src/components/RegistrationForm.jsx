278f2208d412866aa69a31e1c82c42975aa2331a
Dieudonne <dieudonnek077@gmail.com>
2024-09-01 11:30:24 +0200

    Implementing Form Handling in React with Controlled Components and Formik

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,40 @@
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
+import {useState} from 'react'
+import React from 'react'
+function Registration(){
+  const [username,setUsername]= useState('')
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  const formik = useFormik({
+    initialValues:{
+      username:'',
+      email:'',
+      password:''
+    },
+
+    validationSchema:Yup.object({
+      username:Yup.string().required('User name is required'),
+      email:Yup.string().required('Invalid email'),
+      password:Yup.string().required('Fill password')
+    }),
+
+    onSubmit:(values)=>{
+      console.log('form submitted',values)
+    }
+
+   })
+  return(
+   <>
+   <form onSubmit={formik.handleSubmit}>
+
+    <input type="text" name='username' placeholder="username"  onChange={formik.handleChange}/>
+    <input type="email" name='email' placeholder="email" onChange={formik.handleChange}/>
+    <input type="password" name='password' placeholder="password" onChange={formik.handleChange}/>
+    <button type='submit'>Submit</button>
+   </form>
+   </>
+  );
+}
+
+export default Registration;
