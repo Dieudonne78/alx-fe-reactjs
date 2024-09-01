63bafc174601a158e9606ebf5a9429f9475a1929
Dieudonne <dieudonnek077@gmail.com>
2024-09-01 16:48:59 +0200

    Advanced Data Handling with React Query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient,QueryClientProvider } from 'react-query'
-import PostsComponent from './Components/PostsComponent'
-
-
+import PostsComponent from './components/PostsComponent'
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <PostsComponent />
+      <PostsComponent/>
     </QueryClientProvider>
   );
 }
