83cffdc5389dc83d676a54b131a871ccd8039302
Dieudonne <dieudonnek077@gmail.com>
2024-09-01 17:08:47 +0200

    Advanced Data Handling with React Query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -6,7 +6,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <PostsComponent/>
+      <PostsComponent />
     </QueryClientProvider>
   );
 }
