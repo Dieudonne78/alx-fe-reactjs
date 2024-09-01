da179965a4b5adf96056b8a74e56af3d575ae14f
Dieudonne78 <dieudonnek077@gmail.com>
2024-09-01 17:47:54 +0200

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useQuery,useQueryClient } from "react-query";
 
 function PostsComponent(){
-  const {isPending,error,data}=useQuery({
+  const {isLoading,isError,fetchPosts}=useQuery({
     queryKey:'fetch-post',
     queryFn:()=> fetch('https://jsonplaceholder.typicode.com/posts')
       .then((res)=>res.json())
