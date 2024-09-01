9be42be4bfca7bd27d8009a0560086bcd69181d0
Dieudonne <dieudonnek077@gmail.com>
2024-09-01 20:48:09 +0200

    Implementing and Testing a Todo List Component in React

--- react-todo/src/components/AddTodoForm.jsx
+++ react-todo/src/components/AddTodoForm.jsx
@@ -1,12 +1,26 @@
-function AddTodoForm(){
-  return(
-    <>
-    <form>
-      <input type="text" />
-      <button>Add todo</button>
+// AddTodoForm.js
+import React, { useState } from 'react';
+
+function AddTodoForm({ onAddTodo }) {
+  const [todoName, setTodoName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTodo(todoName);
+    setTodoName(''); // Clear the input after submission
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="todo name"
+        value={todoName}
+        onChange={(e) => setTodoName(e.target.value)}
+      />
+      <button type="submit">Add todo</button>
     </form>
-    </>
-  )
+  );
 }
 
 export default AddTodoForm;
