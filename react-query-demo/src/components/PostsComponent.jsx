da943b4fd0e800720b32d9005a496d63a9999faf
Dieudonne78 <dieudonnek077@gmail.com>
2024-09-01 17:42:36 +0200

    Create PostsComponent.jsx in components directory

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useQuery,useQueryClient } from "react-query";
+
+function PostsComponent(){
+  const {isPending,error,data}=useQuery({
+    queryKey:'fetch-post',
+    queryFn:()=> fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res)=>res.json())
+      .then((response)=>console.log(response))
+  })
+  const queryClient = useQueryClient()
+  const refetchData=()=>{
+   queryClient.invalidateQueries('fetch-post')
+  }
+  return(
+    <>
+    <h2>Fetch post component</h2>
+     <button onClick={refetchData()}></button>
+    </>
+  )
+}
+
+export default PostsComponent;
