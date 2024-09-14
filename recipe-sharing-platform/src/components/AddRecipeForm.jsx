be64ed2d66c23bdb8b494f9b23301ccf0d2f8b9f
Dieudonne <dieudonnek077@gmail.com>
2024-09-14 15:37:08 +0200

    3. Implementing a Responsive Form to Add New Recipes

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -15,11 +15,11 @@ function Addrecipeform(){
   }
 
   return(
-      <form onSubmit={handleSubmit}>
-        <input onChange={(e)=>setTitle(e.target.value)} type="text" id="title" value={title}/>
-        <textarea onChange={(e)=>setIngredients(e.target.value)} type="text" id="ingredients" value={ingredients}></textarea>
-        <textarea onChange={(e)=>setSteps(e.target.value)} type="text" id="steps " value={steps }></textarea>
-        <button type="submit" id="submission" > post the form data.</button>
+      <form className="grid grid-cols-1 grid-cols-2 grid-cols-3 md"onSubmit={handleSubmit}>
+        <input className="shadow-lg" onChange={(e)=>setTitle(e.target.value)} type="text" id="title" value={title}/>
+        <textarea className="rounded-lg" onChange={(e)=>setIngredients(e.target.value)} type="text" id="ingredients" value={ingredients}></textarea>
+        <textarea className="text-sm" onChange={(e)=>setSteps(e.target.value)} type="text" id="steps " value={steps }></textarea>
+        <button className="hover:shadow-2xl" type="submit" id="submission" > post the form data.</button>
         {errors && <small>All fields are required</small>}
       </form>
   )
