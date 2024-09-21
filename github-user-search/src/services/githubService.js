e6546aa51112f9e0b2f64603f739c1556f248476
Dieudonne <dieudonnek077@gmail.com>
2024-09-21 16:35:11 +0200

    2. Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+["https://api.github.com/search/users?q", "location", "minRepos"]
 const url=import.meta.env.VITE_APP_GITHUB_API_KEY
 async function fetchUserData(e){
   e.preventDefault();
