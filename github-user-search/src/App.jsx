42911345e210b49f8fc71478d983fca1bbd83b06
Dieudonne <dieudonnek077@gmail.com>
2024-09-21 10:15:39 +0200

    0. Setting Up a React Project for GitHub User Search Application

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' Component={<Application />}/>
-      <Route path='/Introduction' Component={<Profile />}/>
+      <Route path='/introduction' Component={<Profile />}/>
     </Routes>
   )
 }
